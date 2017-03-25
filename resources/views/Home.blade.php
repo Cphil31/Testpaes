27177e5d2247c67bc4cdaca9ae31737f01a413e8
philippe <cecilephilippe31@gmail.com>
2017-03-25 18:08:25 +0100

    git init

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>home</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.9/semantic.min.css">
+</head>
+<body>
+	<div class="ui grid">
+		<div class="two wide column"></div>
+		<div class="twelve wide column">
+			<div class="ui six item menu">
+				<a class="item"><h3>Acceuil</h3></a>
+				<a class="item"><h3>l'immotique</h3></a>
+				<a class="item"><h3>Dans l'entreprise</h3></a>
+				<a class="item"><h3>Tous concernés</h3></a>
+				<a class="item"><h3>Qui sommes-nous</h3></a>
+				<a class="item"><h3>Contact</h3></a>
+			</div>
+
+			<span>Agence Européenne de Sensibilisation et d'Accompagnement aux Technologies Immotiques</span>
+
+			<span>Association à but non lucratif régie par la loi du 1er Juillet 1901 et le décret du 16 Août 1901</span>
+
+
+			<div class="two wide column"></div>
+		</div>
+
+		<img src="http://placehold.it/1500x300">
+
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores in consequuntur, odit voluptates expedita, laudantium qui, libero ea maxime nemo doloremque obcaecati provident facere aperiam, ducimus. Laudantium animi, autem nam.</p>
+
+		<img src="http://placehold.it/1500x300">
+
+		<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, ut quas necessitatibus sit vitae. Voluptatibus molestias veritatis a magni minus commodi. Sequi eius voluptate, itaque dolore reiciendis blanditiis expedita eveniet?</span>
+
+Vous pouvez aussi nous contacter en cliquant ici 
+
+			<script src="https://cdn.jsdelivr.net/semantic-ui/2.2.9/semantic.min.js"></script>
+		</body>
+		</html>
